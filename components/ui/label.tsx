c3b6623d00f6fe53b6f9f29dfca71dc408074fa5
bismaabid334 <bismaabid334@gmail.com>
2025-07-05 20:47:45 +0500

    Update label.tsx

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { cva, type VariantProps } from "class-variance-authority"
 
-import { cn } from "@/lib/utils"
+import { cn } from '../../lib/utils';
 
 const labelVariants = cva(
   "flex items-center gap-2 text-sm leading-none font-medium select-none transition-all duration-200 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
